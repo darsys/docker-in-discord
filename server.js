2fca5a2c013111c242ddb1b8ff5bbd6908deef37
darsys <damon@ourcomputershop.com>
2021-02-01 07:04:45 +0000

    linting

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 'use strict';
 
-require('dotenv').config()
+require('dotenv').config();
 
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN
-const DISCORD_SERVERID = process.env.DISCORD_SERVERID
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const DISCORD_SERVERID = process.env.DISCORD_SERVERID;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let dcserver 
+let dcserver;
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`)
-	dcserver = client.guilds.resolve(DISCORD_SERVERID)
-	console.log(dcserver.name)
+	dcserver = client.guilds.resolve(DISCORD_SERVERID);
+	console.log(`Logged in as ${client.user.tag}! to server ${dcserver.name}!`);
   });
   
 client.on('message', msg => {
-	dcserver.channels.create(msg.content)
+	dcserver.channels.create(msg.content);
 });
 
 client.login(DISCORD_TOKEN);
